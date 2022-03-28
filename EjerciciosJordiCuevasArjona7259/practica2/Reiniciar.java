975c2c7c281e4ebe93523dc47c770c7e6198dcbb
Jordi7259 <7259@itescam.edu.mx>
2022-03-28 15:09:32 -0600

    main

---
+++ EjerciciosJordiCuevasArjona7259/practica2/Reiniciar.java
@@ -0,0 +1,26 @@
+package EjerciciosJordiCuevasArjona7259.practica2;
+
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Graphics;
+public class Reiniciar extends JButton{
+
+	public Reiniciar(String nombre) {
+		super(nombre);
+		setContentAreaFilled(false);
+	}
+	
+@Override
+protected void paintComponent(Graphics g) {
+	g.setColor(Color.BLUE);
+	g.fillOval(0, 0, getSize().width-1, getSize().height-1);
+	super.paintComponent(g);
+}
+@Override
+protected void paintBorder(Graphics g) {
+	// TODO Auto-generated method stub
+	g.setColor(Color.BLUE);
+	g.drawOval(0, 0, getSize().width-1, getSize().height-1);
+}
+
+}
