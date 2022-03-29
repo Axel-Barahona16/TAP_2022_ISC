7c78f9b67511336dcf42fd06d01b4498f3c40e62
Axel-Barahona16 <7266@itescam.edu.mx>
2022-03-29 00:20:59 -0600

    as

--- EjerciciosAxelBarahonaMena7266/Ejercicio2/VentanaMejorada.java
+++ EjerciciosAxelBarahonaMena7266/Ejercicio2/VentanaMejorada.java
@@ -1,5 +1,7 @@
-import static java.awt.Font.PLAIN;
+package EjerciciosAxelBarahonaMena7266.Ejercicio2;
 
+
+import static java.awt.Font.PLAIN;
 import java.awt.*;
 import java.awt.BorderLayout;
 import java.awt.Button;
@@ -17,7 +19,6 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
