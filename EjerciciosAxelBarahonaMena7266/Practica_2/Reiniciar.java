eae37333384783a66cc5d89567182de362dd8c85
Axel-Barahona16 <7266@itescam.edu.mx>
2022-03-29 15:43:09 -0600

    as

--- EjerciciosAxelBarahonaMena7266/Practica_2/Reiniciar.java
+++ EjerciciosAxelBarahonaMena7266/Practica_2/Reiniciar.java
@@ -4,21 +4,21 @@ import java.awt.Graphics;
 public class Reiniciar extends JButton{
 
 	public Reiniciar(String nombre) {
-		super(nombre);
-		setContentAreaFilled(false);
+		                   super(nombre);
+		     setContentAreaFilled(false);
 	}
 	
 @Override
 protected void paintComponent(Graphics g) {
-	g.setColor(Color.GREEN);
-	g.fillOval(0, 0, getSize().width-1, getSize().height-1);
-	super.paintComponent(g);
+g.setColor(Color.GREEN);
+g.fillOval(0, 0, getSize().width-1, getSize().height-1);
+super.paintComponent(g);
 }
+
 @Override
 protected void paintBorder(Graphics g) {
-	// TODO Auto-generated method stub
-	g.setColor(Color.GREEN)l;
-	g.drawOval(0, 0, getSize().width-1, getSize().height-1);
+g.setColor(Color.GREEN);
+g.drawOval(0, 0, getSize().width-1, getSize().height-1);
 }
 
 }
