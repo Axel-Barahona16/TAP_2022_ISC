208419ebfc23aa1f1921343691edd782874f69a2
Axel-Barahona16 <7266@itescam.edu.mx>
2022-03-29 15:35:36 -0600

    as

--- EjerciciosAxelBarahonaMena7266/Practica_2/VideoJuego.java
+++ EjerciciosAxelBarahonaMena7266/Practica_2/VideoJuego.java
@@ -1,253 +1,180 @@
-package EjerciciosAxelBarahonaMena7266.Practica_2;
-
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class VideoJuego extends JFrame {
 
-    private static final long serialVersionUID = 1L;
+public class Videojuego extends Frame implements MouseMotionListener, MouseListener{
 
-    public VideoJuego() {
-        initComponets();
-    }
+    Label element1;
+    Label element2;
+    Label element3;
+    Label element4;
+    Reiniciar R;
+    Componentes C;
 
-    public void initComponets() {
+    public Videojuego(){
+    
+        //Se crea el primer componente que es el boton
+        R = new Reiniciar("Reiniciar");
+        R.setBounds(420, 470, 90, 90);
+        R.setForeground(Color.BLACK);
+        R.addActionListener(evt ->btnRActionPerformed(evt));
+        
+        //Se crean los componentes dos y tres
+        C = new Componentes();
 
-        this.setLayout(new BorderLayout());
-        this.addKeyListener(new KeyAdapter() {
-            public void keyPressed(KeyEvent e) {
-                paintContainerKeyPressed(e);
-            }
-        });
+        //Se crean los componentes arrastables
+        element1 = new Label("   A   ");
+        element1.setBounds(725, 200, 30, 30);
+        element1.setBackground(Color.YELLOW);
+        element1.addMouseMotionListener(this);
+        element1.addMouseListener(this);
 
-        btnCirculo = new JButton("Circulo (a)");
-        btnCirculo.setFocusable(false);
-        btnCirculo.setFont(new Font("Open Sans", 0, 14));
-        btnCirculo.setPreferredSize(new Dimension(110, 30));
-        btnCirculo.addActionListener(e -> btnCirculoActionPerformed(e));
+        element2 = new Label("   B   ");
+        element2.setBounds(775, 200, 30, 30);
+        element2.setBackground(Color.YELLOW);
+        element2.addMouseMotionListener(this);
+        element2.addMouseListener(this);
 
-        btnCuadrado = new JButton("Cuadrado (s)");
-        btnCuadrado.setFocusable(false);
-        btnCuadrado.setFont(new Font("Open Sans", 0, 14));
-        btnCuadrado.setPreferredSize(new Dimension(120, 30));
-        btnCuadrado.addActionListener(e -> btnCuadradoActionPerformed(e));
+        element3 = new Label("   C   ");
+        element3.setBounds(725, 235, 30, 30);
+        element3.setBackground(Color.YELLOW);
+        element3.addMouseMotionListener(this);
+        element3.addMouseListener(this);
 
-        btnArco = new JButton("Arco (d)");
-        btnArco.setFocusable(false);
-        btnArco.setFont(new Font("Open Sans", 0, 14));
-        btnArco.setPreferredSize(new Dimension(110, 30));
-        btnArco.addActionListener(e -> btnRomboActionPerformed(e));
+        element4 = new Label("   D   ");
+        element4.setBounds(775, 235, 30, 30);
+        element4.setBackground(Color.YELLOW);
+        element4.addMouseMotionListener(this);
+        element4.addMouseListener(this);
 
-        btnColor = new JButton("Color (c)");
-        btnColor.setFocusable(false);
-        btnColor.setFont(new Font("Open Sans", 0, 14));
-        btnColor.setPreferredSize(new Dimension(120, 30));
-        btnColor.addActionListener(e -> btnColorActionPerformed(e));
-
-        btnTamanio = new JButton("Tamaño (t)");
-        btnTamanio.setFocusable(false);
-        btnTamanio.setFont(new Font("Open Sans", 0, 14));
-        btnTamanio.setPreferredSize(new Dimension(120, 30));
-        btnTamanio.addActionListener(e -> btnTamanioActionPerformed(e));
-
-        iconCircle = new JLabel();
-        iconCircle.setIcon(new ImageIcon(getClass().getResource("/practica2/img/imgCircle.png")));
-
-        iconSquare = new JLabel();
-        iconSquare.setIcon(new ImageIcon(getClass().getResource("/practica2/img/imgSquare.png")));
-
-        iconRectangle = new JLabel();
-        iconRectangle.setIcon(new ImageIcon(getClass().getResource("/practica2/img/imgRectangle.png")));
-
-        iconTriangle = new JLabel();
-        iconTriangle.setIcon(new ImageIcon(getClass().getResource("/practica2/img/imgTriangle.png")));
-
-        barraBoton = new JPanel();
-        barraBoton.setBackground(new Color(216, 241, 255));
-        barraBoton.add(btnCirculo);
-        barraBoton.add(btnCuadrado);
-        barraBoton.add(btnArco);
-        barraBoton.add(btnColor);
-        barraBoton.add(btnTamanio);
-
-        scrollPane = new JScrollPane();
-        scrollPane.setBorder(null);
-        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setFocusTraversalPolicyProvider(true);
-        contOpciones = new JPanel();
-        contOpciones.setPreferredSize(new Dimension(90, 400));
-        contOpciones.setBackground(new Color(216, 241, 255));
-        contOpciones.add(iconCircle);
-        contOpciones.add(iconSquare);
-        contOpciones.add(iconRectangle);
-        contOpciones.add(iconTriangle);
-        scrollPane.setViewportView(contOpciones);
-
-        paintContainer = new Paint();
-        paintContainer.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                paintContainerMouseClicked(e);
-            }
-        });
-        paintContainer.addMouseMotionListener(new MouseMotionAdapter() {
-            public void mouseDragged(MouseEvent e) {
-                paintContainerMouseDragged(e);
-            }
-        });
-        paintContainer.addKeyListener(new KeyAdapter() {
-            public void keyPressed(KeyEvent e) {
-                paintContainerKeyPressed(e);
-            }
-        });
-
-        this.add(barraBoton, BorderLayout.NORTH);
-        this.add(scrollPane, BorderLayout.EAST);
-        this.add(paintContainer, BorderLayout.CENTER);
-
-        // Personalizacion de la ventana
-        this.setSize(800, 500);
-        this.setLocationRelativeTo(null);
-        // this.setResizable(false);
+        //Agregamos a la ventana cada uno de los elementos del programa
+        this.add(element1);
+        this.add(element2);
+        this.add(element3);
+        this.add(element4);
+        this.add(R);
+        this.add(C);
+    
+        //Propiedades de la ventana
         this.setVisible(true);
+        this.setResizable(false);
+        this.setSize(900, 800);
+        cerrar();
 
+        
     }
+
+    //Se genera la accion del boton R
+    private void btnRActionPerformed(ActionEvent e){
+		reiniciar();
+	}
+
+    //Metodo para cerrar la ventana
+    public void cerrar() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+    public void mover(MouseEvent g, Label lbl) {
+		lbl.setLocation(
+		        lbl.getX() + g.getX() - lbl.getWidth() / 2,
+		        lbl.getY() + g.getY() - lbl.getHeight() / 2  
+		    );
+	}
+
+    //Se crea metodo para Reiniciar las posiciones
+    public void reiniciar(){
+        element1.setBounds(725, 200, 30, 30);
+        element2.setBounds(775, 200, 30, 30);
+        element3.setBounds(725, 235, 30, 30);
+        element4.setBounds(775, 235, 30, 30);
+    }
+
+    public void validar(Label lbl) {
+		int x = lbl.getX();
+		int y = lbl.getY();
+		if(x>=50 & x<=350 & y>50 & y<350) {
+		}else {
+			reiniciar();
+		}
+		
+	}
 
     public static void main(String[] args) {
-        VideoJuego videojuego = new VideoJuego();
-        videojuego.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        Videojuego v = new Videojuego();
     }
 
-    private void btnCirculoActionPerformed(ActionEvent e) {
-        paintContainer.setIsFigura("Circulo");
-        JOptionPane.showMessageDialog(null, "Circulo");
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 
-    private void btnCuadradoActionPerformed(ActionEvent e) {
-        paintContainer.setIsFigura("Cuadrado");
-        JOptionPane.showMessageDialog(null, "Cuadrado");
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 
-    private void btnRomboActionPerformed(ActionEvent e) {
-        paintContainer.setIsFigura("Arco");
-        JOptionPane.showMessageDialog(null, "Arco");
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        if(e.getSource() == element1) {
+			validar(element1);
+		}if(e.getSource() == element2) {
+			validar(element2);
+		}if(e.getSource() == element3) {
+			validar(element3);
+		}if(e.getSource() == element4) {
+			validar(element4);
+		}
     }
 
-    private void btnColorActionPerformed(ActionEvent e) {
-        try {
-            int c = Integer.parseInt(
-                    JOptionPane.showInputDialog(null, "Elija su Color Segun El Numero\n" + "0 - Black\n" + "1 - Blue\n"
-                            + "2 - White\n" + "3 - Cyan\n" + "4 - Gray\n" + "5 - Green\n" + "6 - Yellow\n"));
-            cambiarColor(c);
-            paintContainer.repaint();
-        } catch (Exception exc) {
-            System.out.println(exc);
-        }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 
-    private void btnTamanioActionPerformed(ActionEvent e) {
-        try {
-            int t = Integer.parseInt(JOptionPane.showInputDialog(null, "Dijite el Tamaño"));
-            paintContainer.setH(t);
-            paintContainer.setW(t);
-            paintContainer.repaint();
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 
-    private void paintContainerMouseClicked(MouseEvent e) {
-        paintContainer.setClicked();
-        paintContainer.setX(e.getX());
-        paintContainer.setY(e.getY());
-        paintContainer.repaint();
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        if(e.getSource() == element1) {
+			mover(e, element1);
+		}
+		if(e.getSource() == element2) {
+			mover(e, element2);
+		}
+		if(e.getSource() == element3) {
+			mover(e, element3);
+		}
+		if(e.getSource() == element4) {
+			mover(e, element4);
+		}
     }
 
-    private void paintContainerMouseDragged(MouseEvent e) {
-        if (!paintContainer.isClicked()) {
-            paintContainer.setX(e.getX());
-            paintContainer.setY(e.getY());
-
-            paintContainer.repaint();
-        }
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
     }
-
-    private void paintContainerKeyPressed(KeyEvent e) {
-        if (!paintContainer.isClicked()) {
-            switch (e.getKeyCode()) {
-            case 'a':
-            case 'A':
-                btnCirculo.doClick();
-                break;
-            case 's':
-            case 'S':
-                btnCuadrado.doClick();
-                break;
-            case 'd':
-            case 'D':
-                btnArco.doClick();
-                break;
-            case 'c':
-            case 'C':
-                btnColor.doClick();
-                break;
-            case 't':
-            case 'T':
-                btnTamanio.doClick();
-                break;
-            default:
-                break;
-            }
-        }
-    }
-
-    public void cambiarColor(int c) {
-        switch (c) {
-        case 0:
-            paintContainer.setColor(Color.BLACK);
-            break;
-        case 1:
-            paintContainer.setColor(Color.BLUE);
-            break;
-        case 2:
-            paintContainer.setColor(Color.WHITE);
-            break;
-        case 3:
-            paintContainer.setColor(Color.CYAN);
-            break;
-        case 4:
-            paintContainer.setColor(Color.GRAY);
-            break;
-        case 5:
-            paintContainer.setColor(Color.GREEN);
-            break;
-        case 6:
-            paintContainer.setColor(Color.YELLOW);
-            break;
-        default:
-            paintContainer.setColor(new Color(0, 132, 246));
-            break;
-        }
-    }
-
-    // Variables declaration
-    JButton btnCirculo;
-    JButton btnCuadrado;
-    JButton btnArco;
-    JButton btnColor;
-    JButton btnTamanio;
-    JLabel iconCircle;
-    JLabel iconSquare;
-    JLabel iconRectangle;
-    JLabel iconTriangle;
-    JPanel barraBoton;
-    Paint paintContainer;
-    JPanel contOpciones;
-    JScrollPane scrollPane;
-    // End of variables declaration
-
+    
 }
