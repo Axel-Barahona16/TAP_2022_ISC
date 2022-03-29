99e9c6a3bd349e948f734ffed91263cd934e511d
Jordi7259 <7259@itescam.edu.mx>
2022-03-29 10:55:20 -0600

    main

--- EjerciciosJordiCuevasArjona7259/practica1/operaciones.java
+++ EjerciciosJordiCuevasArjona7259/practica1/operaciones.java
@@ -5,14 +5,16 @@ public class operaciones {
 	public String opResultado(String operador, int pNum, int sNum){
 
 		switch(operador){
+
+			//suma
 		case"+":
 			int suma= pNum +sNum;
 			return suma+"";
-			
+			//resta
 		case "-":
 		int resta=pNum-sNum;
 		return resta+"";
-		
+		//caso para la division
 		case "/":
 			if (pNum>0 && sNum>0){
 				int divicion=pNum/sNum;
@@ -20,11 +22,11 @@ public class operaciones {
 			}else {
 				return "No se puede dividir entre 0";
 			}
-			
+			//multiplicacion 
 		case "*":
 			int multiplicacion=pNum*sNum;
 			return multiplicacion+"";
-			
+			//elevada
 		case "^":
 			if(sNum !=0){
 				double elevar = Math.pow(pNum, sNum);
